d39bc3701ec2a5f0a81194891da11da8b3afdaa8
Ronaldo <ronaldo2004@gmail.com.com>
2024-04-02 16:23:47 -0300

    dddddddd

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Gerência de Engenharia e Planejamento de Manutenção da Transmissão - AT/PM";
+?>
+<br>
+<br>
+    <a href="atpm/index.html"><b> AT/PM-Acesso a paineis de  Sistemas Especialistas </b></a>
+</body>
+</html> 
